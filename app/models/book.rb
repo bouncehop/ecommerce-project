df25f06099efc09d728b49429170c392f8d69104
Matthew C. Kang <11ctko@gmail.com>
2021-04-06 22:58:15 -0500

    Added validation to book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :category
+  validates :title, :author, :description, :year, :price, presence: true
+  validates :title, uniqueness: true
+  validates :year, :price, numericality: { only_integer: true }
 end
