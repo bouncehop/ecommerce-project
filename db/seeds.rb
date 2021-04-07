1a87359e29cf92208406a8b53656b96048f87764
Matthew C. Kang <11ctko@gmail.com>
2021-04-07 16:15:07 -0500

    Added seed code

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Book.delete_all
+Category.delete_all
+
+10.times do
+  category = Category.create(name: Faker::Book.unique.genre)
+
+  10.times do
+    book = category.books.create(
+      title: Faker::Book.unique.title,
+      author: Faker::Book.author,
+      description: Faker::Lorem.paragraph,
+      year: Faker::Number.between(from: 1940, to: 2021),
+      price: Faker::Number.between(from: 500, to: 1000)
+    )
+  end
+end
+
+
+puts "Created #{Category.count} categories"
+puts "Created #{Book.count} books"
