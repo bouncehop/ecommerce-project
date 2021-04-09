b669f79301296fa17c36edfb097c5ec30a87991d
Matthew C. Kang <11ctko@gmail.com>
2021-04-08 23:47:08 -0500

    Added root to books index in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@
 Rails.application.routes.draw do
   resources :books, only: %i[index show]
   resources :categories, only: %i[index show]
-  get 'categories/index'
-  get 'categories/show'
-  get 'books/index'
-  get 'books/show'
+  root to: "books#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
