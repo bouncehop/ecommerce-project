b6e234144edaf75c36f5162f6326e1241d30e627
Matthew C. Kang <11ctko@gmail.com>
2021-04-10 21:44:54 -0500

    Added active storage and image upload function

--- app/admin/books.rb
+++ app/admin/books.rb
@@ -5,7 +5,16 @@ ActiveAdmin.register Book do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :title, :author, :description, :year, :price, :category_id
+  permit_params :title, :author, :description, :year, :price, :category_id, :image
+
+  form do |f|
+    f.semantic_errors # shows errors on :base
+    f.inputs          # builds an input field for every attribute
+    f.inputs do
+      f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image) : ""
+    end
+    f.actions         # adds the 'Submit' and 'Cancel' buttons
+  end
   #
   # or
   #
--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,5 @@ class Book < ApplicationRecord
   validates :title, :author, :description, :year, :price, presence: true
   validates :title, uniqueness: true
   validates :year, :price, numericality: { only_integer: true }
+  has_one_attached :image
 end
