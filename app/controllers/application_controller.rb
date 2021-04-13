1f5c36282b8bd1cf810304d71f8a36fa38df751f
Matthew C. Kang <11ctko@gmail.com>
2021-04-13 00:26:32 -0500

    Added cart controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :initialize_session
+  helper_method :cart
+
+  private
+
+  def initialize_session
+    session[:shopping_cart] ||= []
+  end
+
+  def cart
+    Book.find(session[:shopping_cart])
+  end
+
 end
