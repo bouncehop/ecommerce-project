aff60cd711fa2474059979b28729a6f23c5ded32
Matthew C. Kang <11ctko@gmail.com>
2021-04-13 01:12:01 -0500

    Added add to cart button

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,10 +1,10 @@
 class CartController < ApplicationController
   def create
-
+    logger.debug("Added #{params[:id]} to cart")
   end
 
   def destroy
-
+    logger.debug("Added #{params[:id]} to cart")
   end
 
 
