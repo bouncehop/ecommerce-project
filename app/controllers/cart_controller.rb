ffe2e89f582050d9779783f2908a2f4045416e6b
Matthew C. Kang <11ctko@gmail.com>
2021-04-13 02:28:46 -0500

    Added create and destroy cart items

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,10 +1,20 @@
 class CartController < ApplicationController
   def create
     logger.debug("Added #{params[:id]} to cart")
+    id = params[:id].to_i
+    session[:shopping_cart] << id
+    book = Book.find(id)
+    flash[:notice] = "🛒 Added \"#{book.title}\" to Cart"
+    redirect_to root_path
   end
 
   def destroy
     logger.debug("Added #{params[:id]} to cart")
+    id = params[:id].to_i
+    session[:shopping_cart].delete(id)
+    book = Book.find(id)
+    flash[:notice] = "🛒 Removed \"#{book.title}\" from Cart"
+    redirect_to root_path
   end
 
 
